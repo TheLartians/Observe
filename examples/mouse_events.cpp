7c0b6ccd66c0bfe2a515e54d08e1dde6375ab99e
Lars Melchior <TheLartians@users.noreply.github.com>
2019-04-07 20:21:37 +0200

    update examples

--- examples/mouse_events.cpp
+++ examples/mouse_events.cpp
@@ -18,14 +18,23 @@ int main(int argc, char **argv) {
   // pseudo-base class can hold any type of observer
   Observer observer_2;
   
-  observer_1.observe(A.clicked,[](float x,float y){ cout << "observer 1 : A clicked at " << x << ", " << y << endl; });
-  observer_2.observe(B.clicked,[](float x,float y){ cout << "observer 2 : B clicked at " << x << ", " << y << endl; });
+  observer_1.observe(A.clicked,[](float x,float y){ 
+    cout << "observer 1 : A clicked at " << x << ", " << y << endl;
+  });
+
+  observer_2.observe(B.clicked,[](float x,float y){
+    cout << "observer 2 : B clicked at " << x << ", " << y << endl;
+  });
   
   // anonymous observer
-  B.clicked.connect([](float x,float y){ cout << "   : B clicked at " << x << ", " << y << endl; });
+  B.clicked.connect([](float x,float y){
+    cout << "   : B clicked at " << x << ", " << y << endl;
+  });
   
   {
-  auto temporary_observer = A.clicked.create_observer([](float x,float y){ cout << "tmp observer: A clicked at " << x << ", " << y << endl; });
+  auto temporary_observer = A.clicked.create_observer([](float x,float y){
+    cout << "tmp observer: A clicked at " << x << ", " << y << endl;
+  });
   cout << "A has two observers, B has two observers" << endl;
   A.mouse_down(1, 0);
   B.mouse_down(0, 1);
